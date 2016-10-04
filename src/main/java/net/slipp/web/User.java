3c3d654d41e9cbf0b0403010ec76f3748665f298
seungdols <seungdols0822@gmail.com>
2016-10-04 22:51:34 +0900

    [add&update] 회원 가입 정보 받기 추가

---
+++ src/main/java/net/slipp/web/User.java
@@ -0,0 +1,47 @@
+package net.slipp.web;
+
+public class User {
+	private String userId;
+	private String userName;
+	private String userEmail;
+	private String userPass;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public String getUserPass() {
+		return userPass;
+	}
+
+	public void setUserPass(String userPass) {
+		this.userPass = userPass;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", userName=" + userName + ", userEmail=" + userEmail + ", userPass="
+				+ userPass + "]";
+	}
+
+}
