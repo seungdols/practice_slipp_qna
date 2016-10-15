502a1a87dc5564eaf8830649419393561e16607d
seungdols <seungdols0822@gmail.com>
2016-10-15 17:03:50 +0900

    HTML 정리 및 URL

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -7,25 +7,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/create")
+    @PostMapping("")
     public String create(User user) {
         System.out.println("User : " + user);
         userRepository.save(user);
-        return "redirect:/list";
+        return "redirect:/user/list";
     }
-	@GetMapping("/list")
     public String list(Model model){
 	    model.addAttribute("users", userRepository.findAll());
-	    return "list";
+	    return "/user/list";
     }
 }
