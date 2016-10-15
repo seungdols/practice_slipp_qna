81385a9cd3b6edd8bbee6dcb35a4ee23d85795bd
seungdols <seungdols0822@gmail.com>
2016-10-15 11:12:54 +0900

    h2 database dependency 추가

---
+++ src/main/java/net/slipp/domain/UserRepository.java
@@ -0,0 +1,12 @@
+package net.slipp.domain;
+
+/**
+ * @PROJECT practice_slipp_qna
+ * @PACKAGE_NAME net.slipp.domain
+ * @NAME seungdols
+ * @DATE 16. 10. 15
+ * @DISCRIPT
+ * @HISTORY
+ */
+public interface UserRepository {
+}
